b170f1b03c2cc966bc1c01bbe088eda481190ec3
Franja24 <108949132+Franja24@users.noreply.github.com>
2022-07-18 09:45:25 -0500

    Add files via upload

---
+++ java_course/src/test/java/chapter_3/QuotaCalculator.java
@@ -0,0 +1,17 @@
+package chapter_3;
+/*
+ * IF ELSE
+ * All salespeople are expected to make at least 10 sales each week.
+ * For those who do, they receive a congratulatory message.
+ * For those who don't, they are informed of how many sales they were short.
+ */
+public class QuotaCalculator {
+    public static void main(String arg[]){
+
+        //Initialize values we know
+
+        //Get values for the unknown
+
+        //Make a decision on the path to take - Output
+    }
+}
